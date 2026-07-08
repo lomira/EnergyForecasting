b458d2faff23d1c14dd3da36d1403d2b053d83e4
lomi <lomir@.com>
2026-07-08 14:49:02 +0000

    Load and validate the Algeria Dataset

--- engine/src/engine/database/manage_conn.py
+++ engine/src/engine/database/manage_conn.py
@@ -1,14 +1,20 @@
 from contextlib import contextmanager
 
 import duckdb
+import pandas as pd
 
 
 @contextmanager
 def duckdb_conn(db_path: str, read_only: bool = False):
-    config = {"read_only": "true" if read_only else "false"}
+    config = {"access_mode": "read_only" if read_only else "read_write"}
     con = duckdb.connect(db_path, config=config)
     try:
         # Pass the connection to the 'with' block
         yield con
     finally:
         con.close()
+
+
+def add_rows(con: duckdb.DuckDBPyConnection, table_name: str, df: pd.DataFrame) -> None:
+    """Add rows to a DuckDB table."""
+    con.execute(f"INSERT INTO {table_name} SELECT * FROM df")
