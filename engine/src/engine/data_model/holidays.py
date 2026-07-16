fe075f75901f8d42e5cbb51847082ca955beff94
lomi <lomir@.com>
2026-07-16 11:25:36 +0000

    Model for TimesSeries

--- engine/src/engine/data_model/holidays.py
+++ engine/src/engine/data_model/holidays.py
@@ -1,22 +1,10 @@
-from datetime import datetime
-
-import pandas as pd
 import pandera.pandas as pa
+
+from engine.data_model.base_schema import BaseTimeSeriesSchema
 
 
 # LOAD MODEL
-class HolidaysSchema(pa.DataFrameModel):
-    datetime: datetime = pa.Field(description="PRIMARY_KEY")
+class HolidaysSchema(BaseTimeSeriesSchema):
     holidays: bool = pa.Field(
         description="Indicates whether the date is a public holiday or not"
     )
-
-    @pa.check("datetime")
-    def check_chronological(cls, s: pd.Series) -> bool:
-        # Returns True if the entire series is chronological
-        return s.is_monotonic_increasing and s.is_unique
-
-    @pa.check("datetime")
-    def check_tz_naive(cls, s: pd.Series) -> bool:
-        # Returns True if the entire series is timezone naive
-        return s.dt.tz is None
